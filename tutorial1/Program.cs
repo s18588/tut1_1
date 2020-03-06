60c33df3a02d4d86141c70bb993af2b3302d7551
s18588 <s18588@pja.edu.pl>
2020-03-06 13:02:22 +0100

    v2

---
+++ tutorial1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace tutorial1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = "https://www.pja.edu.pl";
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(url);
+
+            var content = response.Content;
+            
+            var regex = new Regex(@"[^@]+@[^\.]+\..+");
+
+            var matches = regex.Matches(content.ToString());
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match.ToString());
+            }
+
+                
+        }
+    }
+}
